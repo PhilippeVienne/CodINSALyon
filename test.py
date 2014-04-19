6d697e5124c8947d5ba0409b73cd7df3ec2cd976
Philippe Vienne <Philippe.Vienne@insa-lyon.fr>
2014-04-19 16:08:23 +0200

    Reformat

--- test.py
+++ test.py
@@ -5,6 +5,6 @@
 if __name__ == '__main__':
     nodes = {1, 2, 3}
     neighbours = {1: [2, 3], 2: [1], 3: [1]}
-    most_neighbours = max(nodes, key = lambda n : len(neighbours[n]))
+    most_neighbours = max(nodes, key=lambda n: len(neighbours[n]))
 
     print most_neighbours
